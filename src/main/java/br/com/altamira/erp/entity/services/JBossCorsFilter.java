8aaffb488f28fde3096cbcad2e57f89c925e1e39
Alessandro Holanda <alessandro.holanda@hotmail.com>
2014-05-06 18:23:09 -0300

    fix: resolve issue

--- src/main/java/br/com/altamira/erp/entity/services/JBossCorsFilter.java
+++ src/main/java/br/com/altamira/erp/entity/services/JBossCorsFilter.java
@@ -27,7 +27,8 @@
 package br.com.altamira.erp.entity.services;
 
 import java.io.IOException;
-
+import java.util.Collection;
+import java.util.Enumeration;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -35,27 +36,149 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletRequest;
 
 public class JBossCorsFilter implements Filter {
 
-  public JBossCorsFilter() {
-  }
+	public JBossCorsFilter() {
+	}
 
-  public void init(FilterConfig fc) throws ServletException {
-  }
+	public void init(FilterConfig fc) throws ServletException {
+	}
 
-  public void destroy() {
-  }
+	public void destroy() {
+	}
 
-  public void doFilter(ServletRequest request, ServletResponse response,
-          FilterChain chain) throws IOException, ServletException {
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
 
-    HttpServletResponse r = (HttpServletResponse) response;
-    r.addHeader("Access-Control-Allow-Origin", "*");
-    r.addHeader("Access-Control-Allow-Headers",
-            "Accept,Accept-Encoding,Accept-Language,Cache-Control,Connection,Content-Length,Content-Type," +
-            "Cookie,Host,Pragma,Referer,RemoteQueueID,User-Agent");
-    r.addHeader("Access-Control-Allow-Credentials", "true");
-    chain.doFilter(request, response);
-  }
+		/*
+		System.out.print("************************ START FILTER: " + ((HttpServletRequest)request).getRequestURI() + " ************************\n");
+		
+		System.out.print("----------------- ServletRequest BEFORE apply cors headers -----------------------------\n");
+
+		Enumeration<String> requestHeaderNames = ((HttpServletRequest)request).getHeaderNames();
+
+		while (requestHeaderNames.hasMoreElements()) {
+
+			String headerName = requestHeaderNames.nextElement();
+
+			Enumeration<String> headers = ((HttpServletRequest)request).getHeaders(headerName);
+			while (headers.hasMoreElements()) {
+				String headerValue = headers.nextElement();
+				System.out.print(headerName + " = " + headerValue + "\t\n");
+			}
+
+		}
+
+		System.out.print("----------------- ServletResponse BEFORE apply cors headers -----------------------------\n");
+
+		Collection<String> responseHeaderNames = ((HttpServletResponse)response).getHeaderNames();
+
+		for (String headerName : responseHeaderNames) {
+
+			Collection<String> headers = ((HttpServletResponse)response).getHeaders(headerName);
+			for (String headerValue : headers) {
+				System.out.print(headerName + " = " + headerValue + "\t\n");
+			}
+
+		}
+
+        System.out.print("------------------ APPLY CORS HEADER -----------------------------\n");
+        */
+        
+		HttpServletResponse r = (HttpServletResponse) response;
+
+		//r.addHeader("Access-Control-Allow-Origin", "*");
+		/*r.addHeader("Access-Control-Allow-Headers",
+				"Accept,Accept-Encoding,Accept-Language,Cache-Control,Connection,Content-Length,Content-Type,"
+						+ "Cookie,Host,Pragma,Referer,RemoteQueueID,User-Agent");*/
+		
+		r.addHeader("Access-Control-Allow-Credentials", "true");
+		r.setHeader("Access-Control-Allow-Origin", "*");
+		r.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+		r.setHeader("Access-Control-Allow-Headers", "Content-Type, X-Requested-With");
+		r.setHeader("Access-Control-Max-Age", "86400");
+
+		r.setHeader("Allow", "GET, HEAD, POST, TRACE, OPTIONS");
+
+		if (!"OPTIONS".equals(((HttpServletRequest)request).getMethod())) {
+			chain.doFilter(request, response);
+		}
+
+		/*System.out.print("----------------- ServletRequest AFTER apply cors headers -----------------------------\n");
+		
+		requestHeaderNames = ((HttpServletRequest)request).getHeaderNames();
+
+		while (requestHeaderNames.hasMoreElements()) {
+
+			String headerName = requestHeaderNames.nextElement();
+
+			Enumeration<String> headers = ((HttpServletRequest)request).getHeaders(headerName);
+			while (headers.hasMoreElements()) {
+				String headerValue = headers.nextElement();
+				System.out.print(headerName + " = " + headerValue + "\t\n");
+
+			}
+
+		}
+        
+		System.out.print("------------------- ServletResponse AFTER apply cors headers -----------------------------\n");
+		
+		responseHeaderNames = ((HttpServletResponse)response).getHeaderNames();
+
+		for (String headerName : responseHeaderNames) {
+
+			Collection<String> headers = ((HttpServletResponse)response).getHeaders(headerName);
+			for (String headerValue : headers) {
+				System.out.print(headerName + " = " + headerValue + "\t\n");
+			}
+
+		}
+
+		System.out.print("************************ END FILTER: " + ((HttpServletRequest)request).getRequestURI() + " ************************\n");
+		*/
+	}
+	
+	public void doOptions(HttpServletRequest request, HttpServletResponse response)
+	        throws IOException {
+		//System.out.print("***** START OPTIONS: " + ((HttpServletRequest)request).getRequestURI() + "-------------------------------\n");
+
+		response.setHeader("Access-Control-Allow-Origin", "*");
+		response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+		response.setHeader("Access-Control-Allow-Headers", "Content-Type, X-Requested-With");
+		response.setHeader("Access-Control-Max-Age", "86400");
+
+		response.setHeader("Allow", "GET, HEAD, POST, TRACE, OPTIONS");
+		
+		//System.out.print("***** END OPTIONS: " + ((HttpServletRequest)request).getRequestURI() + "-------------------------------\n");
+	}
+	
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+	        throws IOException {
+		//System.out.print("***** START GET: " + ((HttpServletRequest)request).getRequestURI() + "-------------------------------\n");
+
+		response.setHeader("Access-Control-Allow-Origin", "*");
+		response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+		response.setHeader("Access-Control-Allow-Headers", "Content-Type, X-Requested-With");
+		response.setHeader("Access-Control-Max-Age", "86400");
+
+		response.setHeader("Allow", "GET, HEAD, POST, TRACE, OPTIONS");
+
+		//System.out.print("***** END GET: " + ((HttpServletRequest)request).getRequestURI() + "-------------------------------\n");
+	}
+	
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+	        throws IOException {
+		//System.out.print("***** START POST: " + ((HttpServletRequest)request).getRequestURI() + "-------------------------------\n");
+
+		response.setHeader("Access-Control-Allow-Origin", "*");
+		response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+		response.setHeader("Access-Control-Allow-Headers", "Content-Type, X-Requested-With");
+		response.setHeader("Access-Control-Max-Age", "86400");
+
+		response.setHeader("Allow", "GET, HEAD, POST, TRACE, OPTIONS");
+		
+		//System.out.print("***** END POST: " + ((HttpServletRequest)request).getRequestURI() + "-------------------------------\n");
+	}
 }
